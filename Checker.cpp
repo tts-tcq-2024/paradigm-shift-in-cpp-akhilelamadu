c6102cbd816175640431151ee95049e994629a9d
Akhil T P <33170852+akhilelamadu@users.noreply.github.com>
2024-08-29 16:27:51 +0530

    Final commit

--- Checker.cpp
+++ Checker.cpp
@@ -20,17 +20,23 @@ public:
         m_ChargeRate(f_ChargeRate),
         m_statusupdated(false)
     {
-        m_TempOk = checkParams(m_Temp,TEMP_MIN,TEMP_MAX);
-        m_SoCOk = checkParams(m_SoC, SOC_MIN, SOC_MAX);
-        m_ChargeRateOk = checkParams(m_ChargeRate, CHARGE_RATE_MIN, CHARGE_RATE_MAX);
+        //checking the individual parameters state
+        m_TempOk        = checkParams(m_Temp,TEMP_MIN,TEMP_MAX);
+        m_SoCOk         = checkParams(m_SoC, SOC_MIN, SOC_MAX);
+        m_ChargeRateOk  = checkParams(m_ChargeRate, CHARGE_RATE_MIN, CHARGE_RATE_MAX);
+        
+        //Checking Overall battery check
         m_BatteryHealth = m_TempOk && m_SoCOk && m_ChargeRateOk;
+
+        //Printing the battery state
         printBatteryHealth();
     }
-    
+
+    //Returns the overall status of the batter
     bool isBatteryOk() const { return m_BatteryHealth; }
     
     
-    //---Range Checkers (pure functions)---//
+    //---Range Checker (pure function)---//
     bool checkParams(float f_parameter, float f_upperLimit, float f_lowerLimit) const
     {
         return (f_parameter < f_upperLimit || f_parameter > f_lowerLimit) ? false : true;
@@ -38,7 +44,7 @@ public:
     //------------------------------------//
 
     // Helper function to print error messages
-    // only the first hit of out of range is printed 
+    // only the first hit is printed (Same as orginal code)
     void printErrorMessage(bool condition, const string& message) {
         if (!condition && !m_statusupdated) {
             cout << message << "\n";
@@ -54,7 +60,10 @@ public:
     }
 
 private:
-    bool m_TempOk, m_SoCOk, m_ChargeRateOk, m_BatteryHealth, m_statusupdated;
+    bool m_TempOk, m_SoCOk, m_ChargeRateOk //individual parameters status
+        , m_BatteryHealth                  //Overall Battery health
+        , m_statusupdated;                 //check out of range is already updated for the battery
+    //member variables to store the charging parameters
     float m_Temp, m_SoC, m_ChargeRate;
 };
 
@@ -65,11 +74,17 @@ int main()
     assert(test1.isBatteryOk() == 1);
     
 
-    BatteryMon test2(50.0f, 85.0f, 0.7f); // Expected: Temperature and SOC out of range
+    BatteryMon test2(50.0f, 85.0f, 0.7f); // Expected: Temperatureout of range
     assert(test2.isBatteryOk() == 0);
 
-    BatteryMon test3(-5.0f, 10.0f, 0.9f); // Expected: All out of range
+    BatteryMon test3(40.0f, 85.0f, 0.7f); // Expected: SOC out of range
     assert(test3.isBatteryOk() == 0);
+
+    BatteryMon test4(40.0f, 40.0f, 0.9f); // Expected: Charge rate out of range
+    assert(test4.isBatteryOk() == 0);
+    
+    BatteryMon test5(-5.0f, 10.0f, 0.9f); // Expected: All out of range, but only Temp will be printed
+    assert(test5.isBatteryOk() == 0);
 
     cout << "All unit tests passed!\n";
     return 0;
